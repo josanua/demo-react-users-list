b7ee4e7f79acef261c0e729d5ca26a287f73463b
Andrew <designwebmoldova@gmail.com>
2023-06-05 09:57:13 +0300

    Add App component state and work with server connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
 // Users list: https://reqres.in/api/users
 
+
+
 function App() {
+  const serverUserList = 'https://reqres.in/api/users';
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+      fetch(serverUserList)
+          .then((res)=> res.json())
+          .then((json) => {
+              setUsers(json.data);
+          })
+          .catch((err) => {
+                  console.log(err);
+                  alert('Error with data server.')
+              }
+          )}
+      , []);
+
   return (
     <div className="App">
       <Users />
